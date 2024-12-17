95093b47712e1dd9fb398aa2de3febf0c6117091
Sridhar Ranganaboina <63110860+Sridhar-Ranganaboina@users.noreply.github.com>
2024-12-17 13:46:48 +0530

    Update ActionExecutorService.py

--- ActionExecutorService.py
+++ ActionExecutorService.py
@@ -5,45 +5,62 @@
 <div style="@(Model.Orientation == "horizontal" ? "display: flex; gap: 15px;" : "display: block;")">
 
     <!-- Kendo Dropdown -->
-    <kendo-dropdownlist name="@Model.FieldName" 
+    <kendo-dropdownlist name="@Model.FieldName"
                         id="searchTypeDropdown"
                         value="@Model.SearchType">
         <datasource data="@Model.SearchTypes" />
     </kendo-dropdownlist>
 
-    <!-- DatePickers -->
-    <div id="inBetweenDates" style="display: @(Model.SearchType == "in-between" ? "block" : "none");">
-        <kendo-datepicker name="fromDate" style="width: 20%;" 
-                          value="DateTime.Now" date-input="true">
-        </kendo-datepicker>
-        <kendo-datepicker name="toDate" style="width: 20%;" 
-                          value="DateTime.Now" date-input="true">
-        </kendo-datepicker>
-    </div>
-
-    <div id="singleDate" style="display: @(Model.SearchType != "in-between" ? "block" : "none");">
-        <kendo-datepicker name="singleDate" style="width: 20%;" 
-                          value="DateTime.Now" date-input="true">
-        </kendo-datepicker>
-    </div>
-
+    <!-- Kendo DatePickers -->
+    @if (Model.SearchType == "in-between")
+    {
+        <kendo-datepicker name="fromDate" id="fromDate" style="width: 20%;"
+                          value="DateTime.Now" date-input="true"></kendo-datepicker>
+        <kendo-datepicker name="toDate" id="toDate" style="width: 20%;"
+                          value="DateTime.Now" date-input="true"></kendo-datepicker>
+    }
+    else
+    {
+        <kendo-datepicker name="singleDate" id="singleDate" style="width: 20%;"
+                          value="DateTime.Now" date-input="true"></kendo-datepicker>
+    }
 </div>
 
 <script>
     $(document).ready(function () {
-        // Bind to the dropdownlist change event
-        $("#searchTypeDropdown").kendoDropDownList({
-            change: function (e) {
-                var selectedValue = this.value();
+        var dropdown = $("#searchTypeDropdown").data("kendoDropDownList");
 
-                if (selectedValue === "in-between") {
-                    $("#inBetweenDates").show();
-                    $("#singleDate").hide();
-                } else {
-                    $("#inBetweenDates").hide();
-                    $("#singleDate").show();
-                }
+        dropdown.bind("change", function () {
+            var selectedValue = this.value();
+
+            // Check which widgets to show/hide
+            if (selectedValue === "in-between") {
+                // Enable 'fromDate' and 'toDate' pickers
+                enableDatePicker("#fromDate");
+                enableDatePicker("#toDate");
+                disableDatePicker("#singleDate");
+            } else {
+                // Enable 'singleDate' picker
+                enableDatePicker("#singleDate");
+                disableDatePicker("#fromDate");
+                disableDatePicker("#toDate");
             }
         });
+
+        function enableDatePicker(id) {
+            var datePicker = $(id).data("kendoDatePicker");
+            if (datePicker) {
+                datePicker.enable(true);
+                $(id).parent().show(); // Show it visually
+            }
+        }
+
+        function disableDatePicker(id) {
+            var datePicker = $(id).data("kendoDatePicker");
+            if (datePicker) {
+                datePicker.enable(false);
+                $(id).parent().hide(); // Hide it visually
+            }
+        }
     });
 </script>
