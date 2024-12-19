930fd95e6861934aaf7683938a1464df8f07d319
Sridhar Ranganaboina <63110860+Sridhar-Ranganaboina@users.noreply.github.com>
2024-12-19 22:38:53 +0530

    Update ActionExecutorService.py

--- ActionExecutorService.py
+++ ActionExecutorService.py
@@ -1,3 +1,6 @@
-<link href=""https://kendo.cdn.telerik.com/themes/10.0.1/default/default-main.css"" rel=""stylesheet"" />
-<script src=""https://code.jquery.com/jquery-3.7.0.min.js""></script>    
-<script src=""https://kendo.cdn.telerik.com/2023.3.1114/js/kendo.all.min.js""></script>
+public static class DateFormats
+{
+    public const string MMDDYYYY = "MM/dd/yyyy";
+    public const string MMYYYY = "MM/yyyy";
+    public const string YYYY = "yyyy";
+}
