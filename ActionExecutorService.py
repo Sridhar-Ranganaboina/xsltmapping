288ce1cf0edd34aab546f5c7c5149c31d4d50a7a
Sridhar Ranganaboina <63110860+Sridhar-Ranganaboina@users.noreply.github.com>
2024-12-19 20:34:36 +0530

    Update ActionExecutorService.py

--- ActionExecutorService.py
+++ ActionExecutorService.py
@@ -1,62 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-   
-    
-    <title></title>
-    <link href="https://kendo.cdn.telerik.com/themes/10.0.1/default/default-main.css" rel="stylesheet" />
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>    
-    <script src="https://kendo.cdn.telerik.com/2024.4.1112/js/kendo.all.min.js"></script>
-    
-    
-</head>
-<body>
-  <div style="display:flex">  
-  <input id="dropDownList" fill-mode="FillMode.Outline" rounded="Rounded.Small" style="width:35%;background-color:ButtonFace"></input>  
-  <input id="fromDateTxt" fill-mode="FillMode.Outline" rounded="Rounded.Small" style="width:35%;background-color:ButtonFace"></input>  
-		 <input id="toDateTxt" fill-mode="FillMode.Outline" rounded="Rounded.Small" style="width:35%;background-color:ButtonFace" />
-			<input id="fromDatepicker" value="10/10/2011" title="datepicker" fill-mode="FillMode.Outline" rounded="Rounded.Small" style="width:35%;background-color:ButtonFace"/>
-    <input id="toDatePicker" value="10/10/2011" title="datepicker" fill-mode="FillMode.Outline" rounded="Rounded.Small" style="width:35%;background-color:ButtonFace"/>
-	<input id="singleDatePicker" value="10/10/2011" title="datepicker" fill-mode="FillMode.Outline" rounded="Rounded.Small" style="width:35%;background-color:ButtonFace"/>
-	
-	</div>
-  
-    <script>
-        $(document).ready(function () {
-		
-			 $("#fromDateTxt").kendoMaskedTextBox({
-            mask: "00/0000"
-        });
-		$("#toDateTxt").kendoMaskedTextBox({
-            mask: "00/0000"
-        });
-		
-            $("#fromDatepicker").kendoDatePicker({
-                componentType: "modern"
-            });
-          $("#toDatePicker").kendoDatePicker({
-                componentType: "modern"
-            });
-			
-			 $("#singleDatePicker").kendoDatePicker({
-                componentType: "modern"
-            });
-			
+string htmlContent = @"
+<div style=""display:flex"">   
+  <input id=""dropDownList"" fill-mode=""FillMode.Outline"" rounded=""Rounded.Small"" style=""width:35%;background-color:ButtonFace""></input>  
+  <input id=""fromDateTxt"" fill-mode=""FillMode.Outline"" rounded=""Rounded.Small"" style=""width:35%;background-color:ButtonFace""></input>  
+  <input id=""toDateTxt"" fill-mode=""FillMode.Outline"" rounded=""Rounded.Small"" style=""width:35%;background-color:ButtonFace""></input>
+  <input id=""fromDatepicker"" value=""10/10/2011"" title=""datepicker"" fill-mode=""FillMode.Outline"" rounded=""Rounded.Small"" style=""width:35%;background-color:ButtonFace"" />
+  <input id=""toDatePicker"" value=""10/10/2011"" title=""datepicker"" fill-mode=""FillMode.Outline"" rounded=""Rounded.Small"" style=""width:35%;background-color:ButtonFace"" />
+  <input id=""singleDatePicker"" value=""10/10/2011"" title=""datepicker"" fill-mode=""FillMode.Outline"" rounded=""Rounded.Small"" style=""width:35%;background-color:ButtonFace"" />
+</div>
 
-            $("#dropDownList").kendoDropDownList({
-                dataSource: ["equal", "in-between"],
-                value: "modern",
-                change: function (e) {
-                    var picker = $("#datepicker").data("kendoDatePicker");
-                    var type = this.value();
+<script>
+    $(document).ready(function () {{
+        $(\"#fromDateTxt\").kendoMaskedTextBox({{
+            mask: \"00/0000\"
+        }});
+        $(\"#toDateTxt\").kendoMaskedTextBox({{
+            mask: \"00/0000\"
+        }});
 
-                   
-                }
-            });
-        });
-    </script>
+        $(\"#fromDatepicker\").kendoDatePicker({{
+            componentType: \"modern\"
+        }});
+        $(\"#toDatePicker\").kendoDatePicker({{
+            componentType: \"modern\"
+        }});
 
-<div>
+        $(\"#singleDatePicker\").kendoDatePicker({{
+            componentType: \"modern\"
+        }});
 
-</body>
-</html>
+        $(\"#dropDownList\").kendoDropDownList({{
+            dataSource: [\"equal\", \"in-between\"],
+            value: \"modern\",
+            change: function (e) {{
+                var picker = $(\"#datepicker\").data(\"kendoDatePicker\");
+                var type = this.value();
+            }}
+        }});
+    }});
+</script>";
